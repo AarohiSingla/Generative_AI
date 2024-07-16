0cd0bc8486431982f1be6d5d3e4a0dd95fa78aa4
Aarohi <aarohisingla1987@gmail.com>
2024-07-16 15:30:16 +0545

    a

---
+++ L-4/chatgpt_clone/chatgpt_like_app.py
@@ -0,0 +1,56 @@
+import os
+import streamlit as st
+import pickle
+import time
+from langchain_openai import OpenAI
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import UnstructuredURLLoader
+from langchain_openai import OpenAIEmbeddings
+
+
+from openai import OpenAI
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
+st.title("ChatGPT-like clone")
+
+# Set OpenAI API key from Streamlit secrets
+client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+
+# Set a default model
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"  #  we've added a default model to st.session_state
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+        
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)  
+    st.session_state.messages.append({"role": "assistant", "content": response})
