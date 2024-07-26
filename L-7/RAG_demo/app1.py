31e54971e4498cd3913c753de3259176b1629118
Aarohi <aarohisingla1987@gmail.com>
2024-07-26 16:43:17 +0545

    a

---
+++ L-7/RAG_demo/app1.py
@@ -0,0 +1,65 @@
+import streamlit as st
+import time
+from langchain_openai import OpenAI
+from langchain_community.document_loaders import UnstructuredURLLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+st.title("RAG Application")
+
+urls = ['url1','url2']
+
+loader = UnstructuredURLLoader(urls=urls)
+data = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000)
+docs = text_splitter.split_documents(data)
+
+all_splits = docs
+vectorstore = Chroma.from_documents(documents=all_splits, embedding=OpenAIEmbeddings())
+
+retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 6})
+
+#retrieved_docs = retriever.invoke("What kind of services they provide?")
+
+llm = OpenAI(temperature=0.4, max_tokens=500)
+
+
+query = st.chat_input("Say something: ") 
+prompt = query
+
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+)
+
+if query:
+    question_answer_chain = create_stuff_documents_chain(llm, prompt)
+    rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
+    response = rag_chain.invoke({"input": query})
+    #print(response["answer"])
+
+    st.write(response["answer"])
+
+
