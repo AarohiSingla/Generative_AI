3d83e91f9ede84abdbf67bdff2bd0512d2797327
Aarohi <aarohisingla1987@gmail.com>
2025-01-29 11:03:13 +0545

    a

---
+++ deepseek-r1_langchain/1.py
@@ -0,0 +1,22 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama.llms import OllamaLLM
+import streamlit as st
+
+
+st.title("Langchain-DeepSeek-R1 app")
+
+template = """Question: {question}
+
+Answer: Let's think step by step."""
+
+prompt = ChatPromptTemplate.from_template(template)
+
+#model = OllamaLLM(model="llama3.1")
+model = OllamaLLM(model="deepseek-r1")
+
+chain = prompt | model
+
+
+question = st.chat_input("Enter your question here")
+if question: 
+    st.write(chain.invoke({"question": question}))
---
+++ deepseek-r1_langchain/README.md
@@ -0,0 +1,26 @@
+### Video Tutorial: 
+
+Download and install  ollama  - https://ollama.com/
+
+## Environment setup:
+
+		Install packages
+
+		pip install --upgrade langchain langchain-community 
+
+
+		#### Provides access to the ollama models.
+		pip install -U langchain-ollama
+		
+		#### Install streamlit.
+		pip install streamlit
+
+
+		#### To run this code- Open command prompt and type
+		streamlit run 1.py
+
+ 
+
+
+
+
